984958f31c30954ac7de0c417eafbe5a5a8418b3
Maksym Khomenko <mv_2010@i.ua>
2024-10-29 20:49:06 +0200

    Add text commit

--- src/com/homen/add/Main.java
+++ src/com/homen/add/Main.java
@@ -3,6 +3,8 @@ package com.homen.add;
 public class Main {
            public static void main(String[] args) {
                System.out.println("My git app");
+               
+               System.out.println("This is shop-online app");
 
            }
 }
